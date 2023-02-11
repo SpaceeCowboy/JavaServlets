0098e3cc08999868be26f8c0f7da860fac3c3a79
Alexey Shapilov <lshapilov@bk.ru>
2023-02-11 19:09:37 +0300

    homework

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,36 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+public class PostRepository {
+  private final ConcurrentHashMap<Long,Post> rep = new ConcurrentHashMap<>();
+  private final AtomicLong count = new AtomicLong();
+
+  public ConcurrentHashMap<Long,Post> all() {
+    return rep;
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(rep.get(id));
+  }
+
+  public Post save(Post post) {
+    if(post.getId() == 0){
+      post.setId(count.incrementAndGet());
+      return rep.put(post.getId(),post);
+    }else if(post.getId() != 0){
+      return rep.put(post.getId(),post);
+    }
+    return null;
+  }
+
+
+  public void removeById(long id) {
+    rep.remove(id);
+  }
+}
